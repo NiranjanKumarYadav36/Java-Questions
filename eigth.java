23268374d840f828ea50977fd0ba07105ce9e396
Niranjan Kumar Yadav <140634765+NiranjanKumarYadav36@users.noreply.github.com>
2023-11-06 21:06:53 +0530

    Update eigth.java

--- eigth.java
+++ eigth.java
@@ -1,52 +1,60 @@
-class NumberPrinter extends Thread{
-
+class NumberPrinter extends Thread {
     private int start;
     private int end;
-    private boolean isEven;
+    private boolean printEven;
 
-    NumberPrinter(int start, int end, boolean isEven){
+    public NumberPrinter(int start, int end, boolean printEven) {
         this.start = start;
         this.end = end;
-        this.isEven = isEven;
+        this.printEven = printEven;
     }
 
     @Override
-    public void run(){
-        for(int i = start; i<=end; i++){
-        if((i%2 == 0)== isEven){
-                System.out.println(+i"");
+    public void run() {
+        for (int i = start; i <= end; i++) {
+            if (printEven && i % 2 == 0) {
+                System.out.print(i + " ");
+            } else if (!printEven && i % 2 != 0) {
+                System.out.print(i + " ");
             }
-        } 
+        }
     }
 }
 
-public class eigth{
-    public static void main(String[] args){
-        System.out.println("Welcome to the Even and Odd Number Printer");
-
-        int start,end;
+public class practice {
+    public static void main(String[] args) {
         java.util.Scanner scanner = new java.util.Scanner(System.in);
-        System.out.println("enter the starting number");
-        start = scanner.nextInt();
-        System.out.println("enter the ending number");
-        end = scanner.nextInt();
 
+        System.out.println("Welcome to the Even and Odd Number Printer!");
 
+        System.out.print("Enter the starting number: ");
+        int start = scanner.nextInt();
+
+        System.out.print("Enter the ending number: ");
+        int end = scanner.nextInt();
 
         NumberPrinter evenPrinter = new NumberPrinter(start, end, true);
         NumberPrinter oddPrinter = new NumberPrinter(start, end, false);
 
+        System.out.print("Even numbers: ");
         evenPrinter.start();
-        oddPrinter.start();
 
-
-        try{
+        try {
             evenPrinter.join();
-            oddPrinter.join();
-        }catch(InterruptedException e){
-            System.out.println("Thread Interrupted.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
 
-        System.out.println("\n program finished");
+        System.out.print("\nOdd numbers: ");
+        oddPrinter.start();
+
+        try {
+            oddPrinter.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println();
+        scanner.close();
     }
 }
